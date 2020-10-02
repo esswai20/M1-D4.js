8e209500dcfe76190ce30038ccdaa3301ddaefc4
esswai20 <68034948+esswai20@users.noreply.github.com>
2020-10-02 13:48:06 +0100

    Added ex 1

---
+++ index1.js
@@ -0,0 +1,9 @@
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculate the area of the rectangle.
+*/
+
+
+const area = function (l1 , l2){
+    return (l1 * l2)
+}
+console.log(area(2, 3))
